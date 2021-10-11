6d5f809130f076dab45f28405b085197599cf14a
MiltonZiehlsdorff <miltonziehlsdorff@gmail.com>
2021-10-11 12:00:36 -0300

    Arquivo .env.production para aplicação Vue

---
+++ compose.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# ./compose.sh "up -d" .env.prod
+
+# $0 = ./compose.sh
+# $1 = "up -d"
+# $2 = .env.prod
+
+echo "Running docker-compose $1"
+
+yes | cp $2 .env
+
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml $1
+
